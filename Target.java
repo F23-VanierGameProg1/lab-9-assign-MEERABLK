d6c83c0040fdf2129b9d691226d968aefa6e6d19
MEERABLK <meerabmercury@outlook.com>
2023-11-03 17:00:46 -0400

    I updated act() method in Target class

--- Target.java
+++ Target.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 /**
  * Write a description of class Target here.
@@ -8,12 +9,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Target extends Actor
 {
-    /**
-     * Act - do whatever the Target wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        // Add your action code here.
+        List<CannonBall> balls = getWorld().getObjects(CannonBall.class);
+        for (int i=0; i<balls.size(); i++)
+        {
+            CannonBall ball = balls.get(i);
+            
+            Vector2D targetToBall = new Vector2D(ball.getX() - getX(), ball.getY() - getY());
+            double distance = targetToBall.magnitude();
+            
+            double ballRadius = getImage().getHeight()/2;
+            double targetRadius = getImage().getHeight()/2;
+            
+            if (distance < ballRadius + targetRadius)
+            {
+                setImage("targetDestroyed.png");
+                Greenfoot.playSound("explosion.wav");
+            }
+        }
     }
 }
